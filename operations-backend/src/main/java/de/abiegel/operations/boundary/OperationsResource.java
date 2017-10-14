f6e8fb7b1905443469456da9234a21e827353fdc
André Biegel <andre.biegel@icloud.com>
2017-10-14 21:39:35 +0200

    rest methods

--- operations-backend/src/main/java/de/abiegel/operations/boundary/OperationsResource.java
+++ operations-backend/src/main/java/de/abiegel/operations/boundary/OperationsResource.java
@@ -1,13 +1,29 @@
 package de.abiegel.operations.boundary;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
 import de.abiegel.operations.entity.Operation;
 
 @Path("operation")
 public class OperationsResource {
-	private List<Operation> store;
+	
+	private List<Operation> store = new ArrayList<>();
+	
+	@GET
+	public List<Operation> currentOperations(){
+		return store;
+	}
+	
+	@PUT
+	public Operation add(Operation operation) {
+		operation.setId(Integer.valueOf(42));
+		this.store.add(operation);
+		return operation;
+	}
 	
 }
