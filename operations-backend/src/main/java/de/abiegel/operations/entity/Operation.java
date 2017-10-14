035b3e15a44f064ee2a5bd87b9c0a20b972d21fc
André Biegel <andre.biegel@icloud.com>
2017-10-14 21:28:51 +0200

    fluent api bilder for an operation,java 8 ..

--- operations-backend/src/main/java/de/abiegel/operations/entity/Operation.java
+++ operations-backend/src/main/java/de/abiegel/operations/entity/Operation.java
@@ -17,46 +17,5 @@ public class Operation {
 		this.id = id;
 	}
 
-	public static class OperationBuilder {
-		public static class OperationBuilderData {
-			public String heading;
-			public String alertingMessage;
-			public ZonedDateTime start;
-			public Integer id;
-
-			public OperationBuilderData() {
-			}
-		}
-
-		private OperationBuilderData data = new OperationBuilderData();
-
-		public OperationBuilder withHeading(String heading) {
-			this.data.heading = heading;
-			return this;
-		}
-
-		public OperationBuilder withAlertingMessage(String alertingMessage) {
-			this.data.alertingMessage = alertingMessage;
-			return this;
-		}
-
-		public OperationBuilder withStart(ZonedDateTime start) {
-			this.data.start = start;
-			return this;
-		}
-
-		public OperationBuilder withId(Integer id) {
-			this.data.id = id;
-			return this;
-		}
-
-		public Operation build() {
-			return new Operation(data.heading, data.alertingMessage, data.start, data.id);
-		}
-	}
-
-	public static OperationBuilder operation() {
-		return new OperationBuilder();
-	}
 
 }
---
+++ operations-backend/src/main/java/de/abiegel/operations/entity/OperationBuilder.java
@@ -0,0 +1,39 @@
+package de.abiegel.operations.entity;
+
+import java.time.ZonedDateTime;
+
+public class OperationBuilder {
+	private String heading;
+	private String alertingMessage;
+	private ZonedDateTime start;
+	private Integer id;
+
+	public OperationBuilder withHeading(String heading) {
+		this.heading = heading;
+		return this;
+	}
+
+	public OperationBuilder withAlertingMessage(String alertingMessage) {
+		this.alertingMessage = alertingMessage;
+		return this;
+	}
+
+	public OperationBuilder withStart(ZonedDateTime start) {
+		this.start = start;
+		return this;
+	}
+
+	public OperationBuilder withId(Integer id) {
+		this.id = id;
+		return this;
+	}
+
+	public Operation build() {
+		return new Operation(heading, alertingMessage, start, id);
+	}
+	
+	public static OperationBuilder operation() {
+		return new OperationBuilder();
+	}
+
+}
